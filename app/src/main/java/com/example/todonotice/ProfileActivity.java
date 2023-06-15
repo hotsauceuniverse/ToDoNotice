bfe8d421ab0b378a1e1838e9cd9aa985f8a80ebd
hotsauceuniverse <jangsy6976@naver.com>
2023-06-15 18:50:22 +0900

    camera&gallery edit

--- app/src/main/java/com/example/todonotice/ProfileActivity.java
+++ app/src/main/java/com/example/todonotice/ProfileActivity.java
@@ -1,105 +1,20 @@
 package com.example.todonotice;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private static final int REQUEST_CAMERA = 1;
-    private static final int REQUEST_GALLERY = 2;
-
-    private ImageView profileImageView;
-
-
     @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
 
-        profileImageView = findViewById(R.id.profile_image);
-        Log.d("ImageView", "profileImageView: " + profileImageView);
-
-        // 사진 업로드
-        profileImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showImagePickerDialog();
-            }
-        });
     }
 
-    private void showImagePickerDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("프로필 이미지 선택");
-        builder.setItems(new CharSequence[]{"카메라", "앨범"}, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                switch (which) {
-                    case 0:
-                        openCamera();
-                        break;
-                    case 1:
-                        openGallery();
-                        break;
-                }
-            }
-        });
-        builder.show();
-    }
-
-    // 카메라 열기
-    private void openCamera() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, REQUEST_CAMERA);
-        }
-    }
-
-    // 앨범 열기
-    private void openGallery() {
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        intent.setType("image/*");
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), REQUEST_GALLERY);
-    }
-
-    // 이미지 선택 결과 처리
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RESULT_OK) {
-            if(requestCode == REQUEST_CAMERA && data != null) {
-                Bundle extras = data.getExtras();
-                Bitmap imageBitmap = (Bitmap) extras.get("data");
-                profileImageView.setImageBitmap(imageBitmap);
-            } else if (requestCode == REQUEST_GALLERY && data != null) {
-                Uri selectedImage = data.getData();
-                String[] filePathColumn = {MediaStore.Images.Media.DATA};
-
-                Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
-                cursor.moveToFirst();
-
-                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                String imagePath = cursor.getString(columnIndex);
-                cursor.close();
-
-                Bitmap imageBitmap = BitmapFactory.decodeFile(imagePath);
-                profileImageView.setImageBitmap(imageBitmap);
-            }
-        }
-    }
 
 }
+
+
+
 
